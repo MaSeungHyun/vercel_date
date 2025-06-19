c5f8cece37fef088058a1593e5a7068f0395d1bc
MaSeungHyun <tpdlqj0514@gmail.com>
2025-06-20 00:31:36 +0900

    feat: LocaleString을 강제 한국으로 설정

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   const serverDate = new Date();
 
   console.log("pure:", serverDate);
-  console.log("iso:", serverDate.toISOString());
+  console.log("iso:", serverDate.toLocaleString());
   console.log("utc:", serverDate.toString());
-  console.log("local:", serverDate.toLocaleString());
+  console.log("local:", serverDate.toLocaleString("kr-KR"));
 
   return (
     <div className="flex flex-col w-screen h-screen items-center justify-center bg-[#1E1F21] gap-5">
